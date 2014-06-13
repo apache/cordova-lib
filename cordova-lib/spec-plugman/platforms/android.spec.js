bb3284ca8aa65fc85fde99210dcdf68c13dc785c
Martin Bektchiev <martin.bektchiev@telerik.com>
2014-06-13 18:12:02 +0300

    Revert "CB-6698 Resolve android <framework> relative to plugin_dir when custom=true"
    This reverts commit 04588a42067a5ba0d303f4b69f99f44640c3b9e0.

--- cordova-lib/spec-plugman/platforms/android.spec.js
+++ cordova-lib/spec-plugman/platforms/android.spec.js
@@ -137,22 +137,21 @@ describe('android project handler', function() {
             afterEach(function() {
                 android.purgeProjectFileCache(temp);
             });
-            it('with custom=true should update the main and library projects', function() {
+            it('should update the main and library projects', function() {
                 var frameworkElement = { attrib: { src: "LibraryPath", custom: true } };
-                var subDir = path.resolve(dummyplugin, frameworkElement.attrib.src);
+                var subDir = path.resolve(temp, frameworkElement.attrib.src);
                 var mainProjectPropsFile = path.resolve(temp, "project.properties");
                 var subProjectPropsFile = path.resolve(subDir, "project.properties");
 
                 var existsSync = spyOn( fs, 'existsSync').andReturn(true);
                 var writeFileSync = spyOn(fs, 'writeFileSync');
                 var readFileSync = spyOn(fs, 'readFileSync').andCallFake(function (file) {
-                    file = path.normalize(file);
-                    if (file === mainProjectPropsFile) {
+                    if (path.normalize(file) === mainProjectPropsFile) {
                         return '#Some comment\ntarget=android-19\nandroid.library.reference.1=ExistingLibRef1\nandroid.library.reference.2=ExistingLibRef2';
-                    } else if (file === subProjectPropsFile) {
+                    } else if (path.normalize(file) === subProjectPropsFile) {
                         return '#Some comment\ntarget=android-17\nandroid.library=true';
                     } else {
-                        throw new Error("Trying to read from an unexpected file " + file + '\n expected: ' + mainProjectPropsFile + '\n' + subProjectPropsFile);
+                        throw new Error("Trying to read from an unexpected file " + file);
                     }
                 })
                 var exec = spyOn(shell, 'exec');
@@ -161,7 +160,7 @@ describe('android project handler', function() {
                 android.parseProjectFile(temp).write();
 
                 expect(_.any(writeFileSync.argsForCall, function (callArgs) {
-                    return callArgs[0] === mainProjectPropsFile && !!/\nandroid.library.reference.3=.*?LibraryPath/.exec(callArgs[1]);
+                    return callArgs[0] === mainProjectPropsFile && callArgs[1].indexOf('\nandroid.library.reference.3=LibraryPath') > -1;
                 })).toBe(true, 'Reference to library not added');
                 expect(_.any(writeFileSync.argsForCall, function (callArgs) {
                     return callArgs[0] === subProjectPropsFile && callArgs[1].indexOf('\ntarget=android-19') > -1;
