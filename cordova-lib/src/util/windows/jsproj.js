93af6a90557bed198d3ab48375aa4a533d86f011
sgrebnov <v-segreb@microsoft.com>
2014-06-18 17:05:06 +0400

    CB-6970 Share win project files manipulation code between cordova and plugman

--- cordova-lib/src/util/windows/jsproj.js
+++ cordova-lib/src/util/windows/jsproj.js
@@ -121,32 +121,38 @@ jsproj.prototype = {
         this.xml.getroot().append(item);
     },
 
-    removeSourceFile:function(relative_path) {
+    removeSourceFile: function(relative_path) {
+        var isRegexp = relative_path instanceof RegExp;
 
-        // path.normalize(relative_path);// ??
-        relative_path = relative_path.split('/').join('\\');
-        // var oneStep = this.xml.findall('ItemGroup/Content[@Include="' + relative_path + '""]/..');
-
-        var item_groups = this.xml.findall('ItemGroup');
-        for (var i = 0, l = item_groups.length; i < l; i++) {
-            var group = item_groups[i];
-            var files = group.findall('Content');
-            for (var j = 0, k = files.length; j < k; j++) {
-                var file = files[j];
-                if (file.attrib.Include == relative_path) {
-                    // remove file reference
-                    group.remove(0, file);
-                    // remove ItemGroup if empty
-                    var new_group = group.findall('Content');
-                    if(new_group.length < 1) {
-                        this.xml.getroot().remove(0, group);
-                    }
-                    return true;
-                }
-            }
+        if (!isRegexp) {
+            // path.normalize(relative_path);// ??
+            relative_path = relative_path.split('/').join('\\');
         }
-        return false;
+
+        var root = this.xml.getroot();
+        // iterate through all ItemGroup/Content elements and remove all items matched
+        this.xml.findall('ItemGroup').forEach(function(group){
+            // matched files in current ItemGroup
+            var filesToRemove = group.findall('Content').filter(function(item) {
+                if (!item.attrib.Include) return false;
+                return isRegexp ? item.attrib.Include.match(relative_path) :
+                    item.attrib.Include == relative_path;
+            });
+
+            // nothing to remove, skip..
+            if (filesToRemove.length < 1) return;
+
+            filesToRemove.forEach(function(file){
+                // remove file reference
+                group.remove(0, file);
+            });
+            // remove ItemGroup if empty
+            if(group.findall('*').length < 1) {
+                root.remove(0, group);
+            };
+        });
     },
+
     // relative path must include the project file, so we can determine .csproj, .jsproj, .vcxproj...
     addProjectReference:function(relative_path) {
         events.emit('verbose','adding project reference to ' + relative_path);
