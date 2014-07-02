fb3c8d63ff3714723c5525e9b3c6de1157bcb14f
Josh Soref <jsoref@blackberry.com>
2014-07-02 15:13:18 -0400

    CB-7045 (fix up quotes to make jshint happy)

--- cordova-lib/src/plugman/install.js
+++ cordova-lib/src/plugman/install.js
@@ -177,7 +177,7 @@ function callEngineScripts(engines) {
                         engine.currentVersion = null;
                     } else {
                         engine.currentVersion = cleanVersionOutput(stdout, engine.name);
-                        if (engine.currentVersion === "") {
+                        if (engine.currentVersion === '') {
                             events.emit('warn', engine.name +' version check returned nothing ('+ scriptPath +'), continuing anyways.');
                             engine.currentVersion = null;
                         }
