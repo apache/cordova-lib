7fc66be7c7cec46b53b77ec7ab2d52044780e0ad
Michal Mocny <mmocny@gmail.com>
2014-07-03 10:55:29 -0400

    CB-7070 Don't create hooks/ if it already exists

--- cordova-lib/src/cordova/create.js
+++ cordova-lib/src/cordova/create.js
@@ -213,13 +213,16 @@ function create(dir, id, name, cfg) {
         // Create basic project structure.
         shell.mkdir(path.join(dir, 'platforms'));
         shell.mkdir(path.join(dir, 'plugins'));
-        shell.mkdir(path.join(dir, 'hooks'));
 
-        // Add hooks README.md
-        shell.cp(path.join(__dirname, '..', '..', 'templates', 'hooks-README.md'), path.join(dir, 'hooks', 'README.md'));
+        // Add template hooks/ for apps that are missing it
+        if (!custom_hooks) {
+          shell.mkdir(path.join(dir, 'hooks'));
+          // Add hooks README.md
+          shell.cp(path.join(__dirname, '..', '..', 'templates', 'hooks-README.md'), path.join(dir, 'hooks', 'README.md'));
+        }
 
-        var configPath = cordova_util.projectConfig(dir);
         // Add template config.xml for apps that are missing it
+        var configPath = cordova_util.projectConfig(dir);
         if (!fs.existsSync(configPath)) {
             var template_config_xml = path.join(__dirname, '..', '..', 'templates', 'config.xml');
             shell.cp(template_config_xml, configPath);
