5b1d3310de87fbbbd5eba3e1d791a63535030956
motorro <motorro@gmail.com>
2014-07-16 05:43:36 +0400

    Restoring plugin preferences and installation url from config.xml

--- cordova-lib/src/cordova/restore.js
+++ cordova-lib/src/cordova/restore.js
@@ -65,34 +65,69 @@ function installPluginsFromConfigXML(cfg) {
     var plugins_dir = path.join(projectRoot, 'plugins');
 
     var features = cfg.doc.findall('feature');
+    // Seems that we have no way to pass preferences per plugin at the moment
+    // As plugin("add") accepts a single options object
+    // If per-plugin preferences are to be done - install plugins
+    // one-by one rather than in a batch way
+    // Using a single var object for all features may lead to preference conflicts
+    var prefVars = {};
+
     features.forEach(function(feature){
         var params = feature.findall('param');
+        var param;
         var pluginId = '';
+        var pluginUrl = '';
         var pluginVersion = '';
-        for (var i = 0; i < params.length; i++) {
-            if (params[i].attrib.name === 'id') {
-                pluginId = params[i].attrib.value;
+        var i;
+        for (i = 0; i < params.length; i++) {
+            param = params[i];
+            if (param.attrib.name === 'url') {
+                pluginUrl = param.attrib.value;
             }
-            if (params[i].attrib.name === 'version') {
-                pluginVersion = params[i].attrib.value;
+            if (param.attrib.name === 'id') {
+                pluginId = param.attrib.value;
+            }
+            if (param.attrib.name === 'version') {
+                pluginVersion = param.attrib.value;
             }
         }
-        var pluginPath =  path.join(plugins_dir,pluginId);
-        // contents of the plugins folder takes precedence hence
-        // we ignore if the correct version is installed or not.
-        if (pluginId !== '' && !fs.existsSync(pluginPath)) {
-            if ( pluginVersion !== '') {
-                pluginId = pluginId + '@' + pluginVersion;
+
+        // Find plugin preferencies inside features
+        var preferencies = feature.findall('preference');
+        // See above statement on batch installing
+        // var prefVars = {};
+        var pref, prefName, prefVal;
+        for (i = 0; i < preferencies.length; i++) {
+            pref = preferencies[i];
+            prefName = pref.attrib.name;
+            prefVal = pref.attrib.value;
+            // Check format
+            if (typeof prefName === 'string' && typeof prefVal === 'string') {
+                prefVars[prefName] = prefVal;
             }
-            events.emit('log', 'Discovered ' + pluginId + ' in config.xml. Installing to the project');
-            pluginsFromConfig.push(pluginId);
+        }
+
+        if (pluginId !== '') {
+            var pluginPath =  path.join(plugins_dir,pluginId);
+            // contents of the plugins folder takes precedence hence
+            // we ignore if the correct version is installed or not.
+            if (!fs.existsSync(pluginPath)) {
+                if ( pluginVersion !== '') {
+                    pluginId = pluginId + '@' + pluginVersion;
+                }
+                events.emit('log', 'Discovered ' + pluginId + ' in config.xml. Installing to the project');
+                // If URL parameter passed - take as a point from where to install
+                pluginsFromConfig.push(pluginUrl !== '' ? pluginUrl : pluginId);
+            }
         }
     });
 
-    //Use cli instead of plugman directly ensuring all the hooks
+    // Use cli instead of plugman directly ensuring all the hooks
     // to get fired.
+    // Pass preferences as CLI variables. As described above
+    // the single list of variables used for the whole plugin group
     if (pluginsFromConfig.length >0) {
-        return plugin('add', pluginsFromConfig);
+        return plugin('add', pluginsFromConfig, {cli_variables:prefVars});
     }
     return Q.all('No config.xml plugins to install');
 }
