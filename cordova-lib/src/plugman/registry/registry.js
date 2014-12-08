212360740e26d8d931681f123bf1c534ad4af6ea
Murat Sutunc <sutunc@gmail.com>
2014-12-08 15:50:35 -0800

    specify a cache-min-time

--- cordova-lib/src/plugman/registry/registry.js
+++ cordova-lib/src/plugman/registry/registry.js
@@ -34,7 +34,8 @@ var npm = require('npm'),
     events = require('../../events'),
     unpack = require('../../util/unpack'),
     plugmanConfigDir = path.resolve(home, '.plugman'),
-    plugmanCacheDir = path.resolve(plugmanConfigDir, 'cache');
+    plugmanCacheDir = path.resolve(plugmanConfigDir, 'cache'),
+    oneDay = 3600*24;
 
 
 module.exports = {
@@ -222,7 +223,8 @@ function initSettings() {
         cache: plugmanCacheDir,
         registry: 'http://registry.cordova.io',
         logstream: fs.createWriteStream(path.resolve(plugmanConfigDir, 'plugman.log')),
-        userconfig: path.resolve(plugmanConfigDir, 'config')
+        userconfig: path.resolve(plugmanConfigDir, 'config'),
+        'cache-min': oneDay
     });
     return Q(settings);
 }
