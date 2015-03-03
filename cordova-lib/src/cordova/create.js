e70432f2d3ad37506d1824a61d066887dbaf68b7
Michal Mocny <mmocny@gmail.com>
2015-03-03 17:44:29 -0500

    [CB-8286] Update create.js to always require passing in a www

--- cordova-lib/src/cordova/create.js
+++ cordova-lib/src/cordova/create.js
@@ -25,177 +25,156 @@ var path          = require('path'),
     lazy_load     = require('./lazy_load'),
     Q             = require('q'),
     CordovaError  = require('../CordovaError'),
-    ConfigParser = require('../configparser/ConfigParser'),
+    ConfigParser  = require('../configparser/ConfigParser'),
     cordova_util  = require('./util'),
-    validateIdentifier = require('valid-identifier');
-
-var DEFAULT_NAME = 'HelloCordova',
-    DEFAULT_ID   = 'io.cordova.hellocordova';
+    validateIdentifier = require('valid-identifier'),
+    _             = require('underscore');
 
 /**
  * Usage:
  * @dir - directory where the project will be created. Required.
- * @id - app id. Optional, default is DEFAULT_ID.
- * @name - app name. Optional, default is DEFAULT_NAME.
  * @cfg - extra config to be saved in .cordova/config.json
  **/
 // Returns a promise.
 module.exports = create;
-function create(dir, id, name, cfg) {
-    if (!dir ) {
-        return Q.reject(new CordovaError(
-            'At least the dir must be provided to create new project. See `'+cordova_util.binname+' help`.'
-        ));
-    }
+function create(dir, cfg) {
+    return Q.fcall(function() {
+        // Lets check prerequisites first
 
-    if(id && !validateIdentifier(id)) {
-        return Q.reject(new CordovaError(
-            'App id contains a reserved word, or is not a valid identifier.'
-        ));
-    }
-
-    // Massage parameters
-    if (typeof cfg == 'string') {
-        cfg = JSON.parse(cfg);
-    }
-    cfg = cfg || {};
-    id = id || cfg.id || DEFAULT_ID;
-    name = name || cfg.name || DEFAULT_NAME;
-
-    // Make absolute.
-    dir = path.resolve(dir);
-
-    events.emit('log', 'Creating a new cordova project with name "' + name + '" and id "' + id + '" at location "' + dir + '"');
-
-    var www_dir = path.join(dir, 'www');
-
-    // dir must be either empty or not exist at all.
-
-    // dir must be either empty except for .cordova config file or not exist at all..
-    var sanedircontents = function (d) {
-        var contents = fs.readdirSync(d);
-        if (contents.length === 0) {
-            return true;
-        } else if (contents.length == 1) {
-            if (contents[0] == '.cordova') {
-                return true;
+        if (!dir) {
+            throw new CordovaError('At least the dir must be provided to create new project. See `' + cordova_util.binname + ' help`.');
+        }
+        if (!cfg) {
+            cfg = config.read(dir);
+            if (!cfg) {
+                throw new CordovaError('Must provide a project configuration.');
             }
         }
-        return false;
-    };
 
-    if (fs.existsSync(dir) && !sanedircontents(dir)) {
-        return Q.reject(new CordovaError('Path already exists and is not empty: ' + dir));
-    }
+        // Make absolute.
+        dir = path.resolve(dir);
 
-    var symlink = false; // Whether to symlink the www dir instead of copying.
-    var diskConfig = config.read(dir);
-    var wwwLib = cfg.lib && cfg.lib.www || diskConfig.lib && diskConfig.lib.www;
-    if (wwwLib) {
+        // dir must be either empty except for .cordova config file or not exist at all..
+        var sanedircontents = function (d) {
+            var contents = fs.readdirSync(d);
+            if (contents.length === 0) {
+                return true;
+            } else if (contents.length == 1) {
+                if (contents[0] == '.cordova') {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        if (fs.existsSync(dir) && !sanedircontents(dir)) {
+            throw new CordovaError('Path already exists and is not empty: ' + dir);
+        }
+
+        // Massage parameters
+        if (typeof cfg == 'string') {
+            cfg = JSON.parse(cfg);
+        }
+
+        if (cfg.id && !validateIdentifier(cfg.id)) {
+            throw new CordovaError('App id contains a reserved word, or is not a valid identifier.');
+        }
+
+        if (!cfg.lib || !cfg.lib.www) {
+            throw new CordovaError('Must supply a source www path.');
+        }
+
         // This was changed from "uri" to "url", but checking uri for backwards compatibility.
-        wwwLib.url = wwwLib.url || wwwLib.uri;
+        cfg.lib.www.url = cfg.lib.www.url || cfg.lib.www.uri;
         // TODO (kamrik): extend lazy_load for retrieval without caching to allow net urls for --src.
-        wwwLib.version = wwwLib.version || 'not_versioned';
-        wwwLib.id = wwwLib.id || 'dummy_id';
-        symlink  = !!wwwLib.link;
-        // Strip link and url from cfg to avoid them being persisted to disk via .cordova/config.json.
-        cfg = JSON.parse(JSON.stringify(cfg));
-        cfg.lib.www = 0;
-        cfg = JSON.parse(JSON.stringify(cfg).replace(',"www":0', '').replace(/"www":0,?/, '').replace(',"lib":{}', '').replace(/"lib":{},?/, ''));
-    }
-
-    // Update cached version of config.json
-    var origAutoPersist = config.getAutoPersist();
-    config.setAutoPersist(false);
-    config(dir, cfg);
-    config.setAutoPersist(origAutoPersist);
-
-    var p;
-    var www_parent_dir;
-    var custom_config_xml;
-    var custom_merges;
-    var custom_hooks;
-
-    if (wwwLib && wwwLib.url) {
-        events.emit('log', 'Using custom www assets from ' + wwwLib.url);
+        cfg.lib.www.version = cfg.lib.www.version || 'not_versioned';
+        cfg.lib.www.id = cfg.lib.www.id || 'dummy_id';
 
         // Make sure that the source www/ is not a direct ancestor of the
         // target www/, or else we will recursively copy forever. To do this,
         // we make sure that the shortest relative path from source-to-target
         // must start by going up at least one directory or with a drive
         // letter for Windows.
-        var rel_path = path.relative(wwwLib.url, www_dir);
+        var rel_path = path.relative(cfg.lib.www.url, dir);
         var goes_up = rel_path.split(path.sep)[0] == '..';
 
         if (!(goes_up || rel_path[1] == ':')) {
             throw new CordovaError(
-                'Project dir "' +
-                dir +
+                'Project dir "' + dir +
                 '" must not be created at/inside the template used to create the project "' +
-                wwwLib.url + '".'
+                cfg.lib.www.url + '".'
             );
         }
-        if (symlink) {
-            p = Q(wwwLib.url);
-            events.emit('verbose', 'Symlinking custom www assets into "' + www_dir + '"');
+    })
+    .then(function() {
+        // Finally, Ready to start!
+        events.emit('log', 'Creating a new cordova project.');
+    })
+    .then(function() {
+        // Strip link and url from cfg to avoid them being persisted to disk via .cordova/config.json.
+        // TODO: apparently underscore has no deep clone.  Replace with lodash or something.
+        var cfgToPersistToDisk = _.clone(cfg);
+        //delete cfgToPersistToDisk.lib.www;
+        //if (Object.keys(cfgToPersistToDisk.lib).length === 0) delete cfgToPersistToDisk.lib;
+
+        // Update cached version of config.json
+        var origAutoPersist = config.getAutoPersist();
+        config.setAutoPersist(false);
+        config(dir, cfgToPersistToDisk);
+        config.setAutoPersist(origAutoPersist);
+    })
+    .then(function() {
+        if (!!cfg.lib.www.link) {
+            events.emit('verbose', 'Symlinking assets."');
+            return cfg.lib.www.url;
         } else {
-            p = lazy_load.custom({'www': wwwLib}, 'www')
-            .then(function(d) {
-                events.emit('verbose', 'Copying custom www assets into "' + www_dir + '"');
-                return d;
-            });
+            events.emit('verbose', 'Copying assets."');
+            return lazy_load.custom({ 'www': cfg.lib.www }, 'www');
         }
-    } else {
-        // No custom www - use stock cordova-hello-world-app.
-        events.emit('verbose', 'Using stock cordova hello-world application.');
-        p = lazy_load.cordova('www')
-        .then(function(d) {
-            events.emit('verbose', 'Copying stock Cordova www assets into "' + www_dir + '"');
-            return d;
-        });
-    }
+    })
+    .then(function(import_from_path) {
+        if (!fs.existsSync(import_from_path)) {
+            throw new CordovaError('Could not find directory: ' + import_from_path);
+        }
 
-    return p.then(function(www_lib) {
-        if (!fs.existsSync(www_lib)) {
-            throw new CordovaError('Could not find directory: '+www_lib);
-        }
+        var paths = {
+            root: import_from_path,
+            www: import_from_path
+        };
+
         // Keep going into child "www" folder if exists in stock app package.
-        while (fs.existsSync(path.join(www_lib, 'www'))) {
-            www_parent_dir = www_lib;
-            www_lib = path.join(www_lib, 'www');
+        while (fs.existsSync(path.join(paths.www, 'www'))) {
+            paths.root = paths.www;
+            paths.www = path.join(paths.root, 'www');
         }
 
-        // Find if we also have custom merges and config.xml as siblings of custom www.
-        if (www_parent_dir && wwwLib) {
-            custom_config_xml = path.join(www_parent_dir, 'config.xml');
-            if ( !fs.existsSync(custom_config_xml) ) {
-                custom_config_xml = null;
-            }
-            custom_merges = path.join(www_parent_dir, 'merges');
-            if ( !fs.existsSync(custom_merges) ) {
-                custom_merges = null;
-            }
-            custom_hooks = path.join(www_parent_dir, 'hooks');
-            if ( !fs.existsSync(custom_hooks) ) {
-                custom_hooks = null;
-            }
+        if (fs.existsSync(path.join(paths.root, 'config.xml'))) {
+            paths.configXml = path.join(paths.root, 'config.xml');
+        }
+        if (fs.existsSync(path.join(paths.root, 'merges'))) {
+            paths.merges = path.join(paths.root, 'merges');
+        }
+        if (fs.existsSync(path.join(paths.root, 'hooks'))) {
+            paths.hooks = path.join(paths.root, 'hooks');
         }
 
         var dirAlreadyExisted = fs.existsSync(dir);
         if (!dirAlreadyExisted) {
             fs.mkdirSync(dir);
         }
-        if (symlink) {
+
+        if (!!cfg.lib.www.link) {
+            // symlink
             try {
-                fs.symlinkSync(www_lib, www_dir, 'dir');
-                if (custom_merges) {
-                    fs.symlinkSync(custom_merges, path.join(dir, 'merges'), 'dir');
+                fs.symlinkSync(paths.www, path.join(dir, 'www'), 'dir');
+                if (paths.merges) {
+                    fs.symlinkSync(paths.merges, path.join(dir, 'merges'), 'dir');
                 }
-                if (custom_hooks) {
-                    fs.symlinkSync(custom_hooks, path.join(dir, 'hooks'), 'dir');
+                if (paths.hooks) {
+                    fs.symlinkSync(paths.hooks, path.join(dir, 'hooks'), 'dir');
                 }
-                if (custom_config_xml) {
-                    fs.symlinkSync(custom_config_xml, path.join(dir, 'config.xml'));
+                if (paths.configXml) {
+                    fs.symlinkSync(paths.configXml, path.join(dir, 'config.xml'));
                 }
             } catch (e) {
                 if (!dirAlreadyExisted) {
@@ -207,20 +186,20 @@ function create(dir, id, name, cfg) {
                 throw e;
             }
         } else {
-            shell.mkdir(www_dir);
-            shell.cp('-R', path.join(www_lib, '*'), www_dir);
-            if (custom_merges) {
-                var merges_dir = path.join(dir, 'merges');
-                shell.mkdir(merges_dir);
-                shell.cp('-R', path.join(custom_merges, '*'), merges_dir);
+            // copy
+            shell.mkdir(path.join(dir, 'www'));
+            shell.cp('-R', path.join(paths.www, '*'), path.join(dir, 'www'));
+
+            if (paths.merges) {
+                shell.mkdir(path.join(dir, 'merges'));
+                shell.cp('-R', path.join(paths.merges, '*'), path.join(dir, 'merges'));
             }
-            if (custom_hooks) {
-                var hooks_dir = path.join(dir, 'hooks');
-                shell.mkdir(hooks_dir);
-                shell.cp('-R', path.join(custom_hooks, '*'), hooks_dir);
+            if (paths.hooks) {
+                shell.mkdir(path.join(dir, 'hooks'));
+                shell.cp('-R', path.join(paths.hooks, '*'), path.join(dir, 'hooks'));
             }
-            if (custom_config_xml) {
-                shell.cp(custom_config_xml, path.join(dir, 'config.xml'));
+            if (paths.configXml) {
+                shell.cp(paths.configXml, path.join(dir, 'config.xml'));
             }
 
         }
@@ -229,23 +208,16 @@ function create(dir, id, name, cfg) {
         shell.mkdir(path.join(dir, 'platforms'));
         shell.mkdir(path.join(dir, 'plugins'));
 
-        // Add template hooks/ for apps that are missing it
-        if (!custom_hooks) {
-            shell.mkdir(path.join(dir, 'hooks'));
-            // Add hooks README.md
-            shell.cp(path.join(__dirname, '..', '..', 'templates', 'hooks-README.md'), path.join(dir, 'hooks', 'README.md'));
-        }
-
         // Add template config.xml for apps that are missing it
         var configPath = cordova_util.projectConfig(dir);
         if (!fs.existsSync(configPath)) {
-            var template_config_xml = path.join(__dirname, '..', '..', 'templates', 'config.xml');
-            shell.cp(template_config_xml, configPath);
-            // Write out id and name to config.xml
-            var conf = new ConfigParser(configPath);
-            conf.setPackageName(id);
-            conf.setName(name);
-            conf.write();
+            shell.cp(path.join(__dirname, '..', '..', 'templates', 'config.xml'), configPath);
         }
+
+        // Write out id and name to config.xml
+        var conf = new ConfigParser(configPath);
+        if (cfg.id) conf.setPackageName(cfg.id);
+        if (cfg.name) conf.setName(cfg.name);
+        conf.write();
     });
 }
