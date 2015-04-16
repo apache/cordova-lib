b18586b7223bd6ec9c5d85ac02e95862e51e92a8
Steve Gill <stevengill97@gmail.com>
2015-04-16 11:07:45 -0700

    CB-8865 fixed plugman.help()

--- cordova-lib/src/plugman/plugman.js
+++ cordova-lib/src/plugman/plugman.js
@@ -67,6 +67,7 @@ addProperty(plugman, 'uninstall', './uninstall', true);
 addProperty(plugman, 'fetch', './fetch', true);
 addProperty(plugman, 'prepare', './prepare');
 addProperty(plugman, 'prepareBrowserify', './prepare-browserify');
+addProperty(plugman, 'help', './help');
 addProperty(plugman, 'config', './config', true);
 addProperty(plugman, 'owner', './owner', true);
 addProperty(plugman, 'adduser', './adduser', true);
