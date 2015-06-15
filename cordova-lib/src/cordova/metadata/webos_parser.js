f9ce4323eea5431deed146f39251eed41911eb7b
Jason Robitaille <jason.robitaille@lge.com>
2015-06-15 14:49:21 -0700

    Fix edge case where no assets folder exists but assets do (such as largeIcon.png)

--- cordova-lib/src/cordova/metadata/webos_parser.js
+++ cordova-lib/src/cordova/metadata/webos_parser.js
@@ -72,6 +72,7 @@ webos_parser.prototype.update_from_config = function(config) {
         if(index===0 || index===1) {
             return src.substring(index+4);
         } else {
+            shell.mkdir('-p', path.join(www, 'assets'));
             var newSrc = 'assets/' + type + '.png';
             if(type==='icon') {
                 newSrc = 'icon.png';
