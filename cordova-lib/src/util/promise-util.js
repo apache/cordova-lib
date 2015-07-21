717af3af870d6058d6e507eec6cdf7d89fd539aa
Omar Mefire <omefire@gmail.com>
2015-07-21 01:25:59 -0700

    CB-9278: Restoring multiple platforms fails. This closes #266

--- cordova-lib/src/util/promise-util.js
+++ cordova-lib/src/util/promise-util.js
@@ -33,4 +33,21 @@ function Q_chainmap(args, func) {
     });
 }
 
+// Behaves similar to Q_chainmap but gracefully handles failures.
+// When a promise in the chain is rejected, it will call the failureCallback and then continue the processing, instead of stopping
+function Q_chainmap_graceful(args, func, failureCallback) {
+    return Q.when().then(function(inValue) {
+        return args.reduce(function(soFar, arg) {
+            return soFar.then(function(val) {
+                return func(arg, val);
+            }).fail(function(err) {
+                if (failureCallback) {
+                    failureCallback(err);
+                }
+            });
+        }, Q(inValue));
+    });
+}
+
 exports.Q_chainmap = Q_chainmap;
+exports.Q_chainmap_graceful = Q_chainmap_graceful;
