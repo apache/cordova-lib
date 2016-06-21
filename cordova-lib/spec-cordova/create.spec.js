2108aa8fbd43da98b1bf27f97274d0bbd66c1340
carynbear <caryn.tran@berkeley.edu>
2016-06-21 12:52:33 -0700

    CB-11607 breakout cordova-create from cordova-lib
    CB-11412 aliased copy-from to template; added support for www template

--- cordova-lib/spec-cordova/create.spec.js
+++ cordova-lib/spec-cordova/create.spec.js
@@ -30,42 +30,10 @@ var appName = 'TestBase';
 var appId = 'org.testing';
 var project = path.join(tmpDir, appName);
 
-var configSubDirPkgJson = {
+var configBasic = {
     lib: {
         www: {
-            template: true,
-            url: path.join(__dirname, 'fixtures', 'templates', 'withsubdirectory_package_json'),
-            version: ''
-        }
-    }
-};
-
-var configConfigInWww = {
-    lib: {
-        www: {
-            template: true,
-            url: path.join(__dirname, 'fixtures', 'templates', 'config_in_www'),
-            version: ''
-        }
-    }
-};
-
-var configGit = {
-    lib: {
-        www: {
-            url: 'https://github.com/apache/cordova-app-hello-world',
-            template: true,
-            version: 'not_versioned'
-        }
-    }
-};
-
-var configNPM = {
-    lib: {
-        www: {
-            template: true,
-            url: 'cordova-app-hello-world',
-            version: ''
+            template: false
         }
     }
 };
@@ -89,7 +57,7 @@ describe('cordova create checks for valid-identifier', function(done) {
 });
 
 
-describe('create end-to-end', function() {
+describe('create basic test (see more in cordova-create)', function() {
     //this.timeout(240000);
 
     beforeEach(function() {
@@ -123,73 +91,15 @@ describe('create end-to-end', function() {
         // expect(configXml.name()).toEqual('TestBase');
     }
 
-    function checkConfigXml() {
-        // Check if top level dirs exist.
-        var dirs = ['hooks', 'platforms', 'plugins', 'www'];
-        dirs.forEach(function(d) {
-            expect(path.join(project, d)).toExist();
-        });
-        expect(path.join(project, 'hooks', 'README.md')).toExist();
-        
-        //index.js and template subdir folder should not exist (inner files should be copied to the project folder)
-        expect(path.join(project, 'index.js')).not.toExist();
-        expect(path.join(project, 'template')).not.toExist();
-
-        // Check if www files exist.
-        expect(path.join(project, 'www', 'index.html')).toExist();
-        var configXml = new ConfigParser(path.join(project, 'www', 'config.xml'));
-        expect(configXml.packageName()).toEqual(appId);
-        expect(configXml.version()).toEqual('1.0.0');
-
-        // Check that config.xml does not exist outside of www
-        expect(path.join(project, 'config.xml')).not.toExist();
-
-        // Check that we got no package.json
-        expect(path.join(project, 'package.json')).not.toExist();
-
-        // Check that we got the right config.xml from the template and not stock
-        expect(configXml.description()).toEqual('this is the correct config.xml');
-    }
-
-    function checkSubDir() {
-        // Check if top level dirs exist.
-        var dirs = ['hooks', 'platforms', 'plugins', 'www'];
-        dirs.forEach(function(d) {
-            expect(path.join(project, d)).toExist();
-        });
-        expect(path.join(project, 'hooks', 'README.md')).toExist();
-        
-        //index.js and template subdir folder should not exist (inner files should be copied to the project folder)
-        expect(path.join(project, 'index.js')).not.toExist();
-        expect(path.join(project, 'template')).not.toExist();
-
-        // Check if config files exist.
-        expect(path.join(project, 'www', 'index.html')).toExist();
-
-        // Check that config.xml was updated.
-        var configXml = new ConfigParser(path.join(project, 'config.xml'));
-        expect(configXml.packageName()).toEqual(appId);
-        expect(configXml.version()).toEqual('1.0.0');
-
-
-        // Check that we got package.json (the correct one)
-        var pkjson = require(path.join(project, 'package.json'));
-        expect(pkjson.name).toEqual(appName.toLowerCase());
-        expect(pkjson.valid).toEqual('true');
-
-        // Check that we got the right config.xml
-        expect(configXml.description()).toEqual('this is the correct config.xml');
-    }
-
     var results;
     events.on('results', function(res) { results = res; });
 
-    it('should successfully run with Git URL', function(done) {
+    it('should successfully run', function(done) {
         // Call cordova create with no args, should return help.
         Q()
             .then(function() {
                 // Create a real project
-                return cordova.raw.create(project, appId, appName, configGit);
+                return cordova.raw.create(project, appId, appName, configBasic);
             })
             .then(checkProject)
             .fail(function(err) {
@@ -198,156 +108,5 @@ describe('create end-to-end', function() {
             })
             .fin(done);
     }, 60000);
-
-    it('should successfully run with NPM package', function(done) {
-        // Call cordova create with no args, should return help.
-        Q()
-            .then(function() {
-                // Create a real project
-                return cordova.raw.create(project, appId, appName, configNPM);
-            })
-            .then(checkProject)
-            .fail(function(err) {
-                console.log(err && err.stack);
-                expect(err).toBeUndefined();
-            })
-            .fin(done);
-    });
-    
-    it('should successfully run with template not having a package.json at toplevel', function(done) {
-        // Call cordova create with no args, should return help.
-        var config = {
-            lib: {
-                www: {
-                    template: true,
-                    url: path.join(__dirname, 'fixtures', 'templates', 'nopackage_json'),
-                    version: ''
-                }
-            }
-        };
-        Q()
-            .then(function() {
-                // Create a real project
-                return cordova.raw.create(project, appId, appName, config);
-            })
-            .then(checkProject)
-            .then(function(){
-                // Check that we got the right config.xml
-                var configXml = new ConfigParser(path.join(project, 'config.xml'));
-                expect(configXml.description()).toEqual('this is the very correct config.xml');
-            })
-            .fail(function(err) {
-                console.log(err && err.stack);
-                expect(err).toBeUndefined();
-            })
-            .fin(done);
-    });
-    
-    it('should successfully run with template having package.json and no sub directory', function(done) {
-        // Call cordova create with no args, should return help.
-        var config = {
-            lib: {
-                www: {
-                    template: true,
-                    url: path.join(__dirname, 'fixtures', 'templates', 'withpackage_json'),
-                    version: ''
-                }
-            }
-        };
-        Q()
-            .then(function() {
-                // Create a real project
-                return cordova.raw.create(project, appId, appName, config);
-            })
-            .then(checkProject)
-            .fail(function(err) {
-                console.log(err && err.stack);
-                expect(err).toBeUndefined();
-            })
-            .fin(done);
-    });
-    
-    it('should successfully run with template having package.json, and subdirectory, and no package.json in subdirectory', function(done) {
-        // Call cordova create with no args, should return help.
-        var config = {
-            lib: {
-                www: {
-                    template: true,
-                    url: path.join(__dirname, 'fixtures', 'templates', 'withsubdirectory'),
-                    version: ''
-                }
-            }
-        };
-        Q()
-            .then(function() {
-                // Create a real project
-                return cordova.raw.create(project, appId, appName, config);
-            })
-            .then(checkProject)
-            .fail(function(err) {
-                console.log(err && err.stack);
-                expect(err).toBeUndefined();
-            })
-            .fin(done);
-    });
-
-
-    it('should successfully run with template having package.json, and subdirectory, and package.json in subdirectory', function(done) {
-        // Call cordova create with no args, should return help.
-        var config = configSubDirPkgJson;
-        Q()
-            .then(function() {
-                // Create a real project
-                project = project + '1';
-                return cordova.raw.create(project, appId, appName, config);
-            })
-            .then(checkSubDir)
-            .fail(function(err) {
-                console.log(err && err.stack);
-                expect(err).toBeUndefined();
-            })
-            .fin(done);
-    });
-
-    it('should successfully run config.xml in the www folder', function(done) {
-        // Call cordova create with no args, should return help.
-        var config = configConfigInWww;
-        Q()
-            .then(function() {
-                // Create a real project
-                project = project + '2';
-                return cordova.raw.create(project, appId, appName, config);
-            })
-            .then(checkConfigXml)
-            .fail(function(err) {
-                console.log(err && err.stack);
-                expect(err).toBeUndefined();
-            })
-            .fin(done);
-    });
-
-    it('should successfully run with www folder as the template', function(done) {
-        var config = {
-            lib: {
-                www: {
-                    template: true,
-                    url: path.join(__dirname, 'fixtures', 'templates', 'config_in_www', 'www'),
-                    version: ''
-                }
-            }
-        };
-        Q()
-            .then(function() {
-                project = project + '3';
-                return cordova.raw.create(project, appId, appName, config);
-            })
-            .then(checkConfigXml)
-            .fail(function(err) {
-                console.log(err && err.stack);
-                expect(err).toBeUndefined();
-            })
-            .fin(done);
-    });
-
 
 });
