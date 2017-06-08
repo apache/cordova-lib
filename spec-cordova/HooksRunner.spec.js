37120613749c6695a33cf9ea70d6c9f9e9ffbb2f
filmaj <maj.fil@gmail.com>
2017-06-08 18:06:48 -0500

    tweaks to existing tests to get closer to having tests pass after removing lazy loading, including: - bump timeout on test that fetches/unpacks tarballs. also ensure failure cases invoke done so we dont wait in case error conditions get hit.
    - remove wrapper specs, leave todo on how to properly unit test.

--- spec-cordova/HooksRunner.spec.js
+++ spec-cordova/HooksRunner.spec.js
@@ -66,6 +66,13 @@ shell.chmod('-R', 'ug+x', hooksDir);
 shell.chmod('-R', 'ug+x', hooksDirDot);
 shell.chmod('-R', 'ug+x', scriptsDir);
 
+// To get more verbose output to help trace program flow, uncomment the lines below
+// TODO: think about how to factor this out so that it can be invoked from the command line
+//var events = require('cordova-common').events;
+//events.on('log', function(msg) { console.log(msg); });
+//events.on('verbose', function(msg) { console.log(msg); });
+//events.on('warn', function(msg) { console.log(msg); });
+
 describe('HooksRunner', function() {
     var hooksRunner;
     var hookOptions;
@@ -120,6 +127,7 @@ describe('HooksRunner', function() {
         // Add the testing platform.
         cordova.raw.platform('add', [helpers.testPlatform]).fail(function (err) {
             expect(err).toBeUndefined();
+            done();
         }).then(function () {
             // Add the testing plugin
             projectRoot = cordovaUtil.isCordova();
@@ -136,13 +144,14 @@ describe('HooksRunner', function() {
 
             cordova.raw.plugin('add', testPluginFixturePath).fail(function (err) {
                 expect(err && err.stack).toBeUndefined();
+                done();
             }).then(function () {
                 testPluginInstalledPath = path.join(projectRoot, 'plugins', 'com.plugin.withhooks');
                 shell.chmod('-R', 'ug+x', path.join(testPluginInstalledPath, 'scripts'));
                 done();
             });
         });
-    });
+    }, 100000);
 
     describe('fire method', function() {
         beforeEach(function() {
