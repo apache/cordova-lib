61b247fc33dc1fa8defa5c4eb646765be6dd2c44
Raphael von der Grün <raphinesse@gmail.com>
2018-06-14 03:33:51 +0200

    Improve output formatting & clean up code
    * Output files last & delimit them in HEREDOC style
    * Indent all regular blocks
    * Remove empty trailing lines
    * Code cleanup

--- src/cordova/info.js
+++ src/cordova/info.js
@@ -28,79 +28,69 @@ var path = require('path');
 var fs = require('fs');
 var Q = require('q');
 
-// Execute using a child_process exec, for any async command
-function execSpawn (command, args, resultMsg, errorMsg) {
-    return superspawn.spawn(command, args).then(function (result) {
-        return resultMsg + result;
-    }, function (error) {
-        return errorMsg + error;
-    });
+const indent = s => require('indent-string')(s, 2);
+
+function failSafeSpawn (command, args) {
+    return superspawn.spawn(command, args)
+        .catch(err => `ERROR: ${err.message}`);
 }
 
-function getPlatformInfo (platform, projectRoot) {
+function getPlatformInfo (platform) {
     switch (platform) {
     case 'ios':
-        return execSpawn('xcodebuild', ['-version'], 'iOS platform:\n\n',
-            'Error retrieving iOS platform information: ');
+        return failSafeSpawn('xcodebuild', ['-version'])
+            .then(out => 'iOS platform:\n' + indent(out));
     case 'android':
-        return execSpawn('android', ['list', 'target'], 'Android platform:\n\n',
-            'Error retrieving Android platform information: ');
+        return failSafeSpawn('android', ['list', 'target'])
+            .then(out => 'Android platform:\n' + indent(out));
     }
 }
 
 module.exports = function info () {
     // Get projectRoot
     var projectRoot = cordova_util.cdProjectRoot();
-    var output = '';
     if (!projectRoot) {
         return Q.reject(new Error('Current working directory is not a Cordova-based project.'));
     }
 
-    // Array of functions, Q.allSettled
-    return Q.allSettled([
+    const infoPromises = [
         // Get Node version
         Q('Node version: ' + process.version),
         // Get Cordova version
         Q('Cordova version: ' + pkg.version),
-        // Get project config.xml file using ano
-        getProjectConfig(projectRoot),
         // Get list of plugins
         listPlugins(projectRoot),
         // Get Platforms information
-        getPlatforms(projectRoot)
-    ]).then(function (promises) {
-        promises.forEach(function (p) {
-            output += p.state === 'fulfilled' ? p.value + '\n\n' : p.reason + '\n\n';
-        });
-        console.info(output);
-    });
+        getPlatforms(projectRoot),
+        // Get project config.xml file using ano
+        getProjectConfig(projectRoot)
+    ];
+
+    const failSafePromises = infoPromises.map(p => Q(p).catch(err => err));
+    return Q.all(failSafePromises)
+        .then(results => console.info(results.join('\n\n')));
 };
 
 function getPlatforms (projectRoot) {
     var platforms = cordova_util.listPlatforms(projectRoot);
-    if (platforms.length) {
-        return Q.all(platforms.map(function (p) {
-            return getPlatformInfo(p, projectRoot);
-        })).then(function (outs) {
-            return outs.join('\n\n');
-        });
+    if (!platforms.length) {
+        return 'No Platforms Currently Installed';
     }
-    return Q.reject('No Platforms Currently Installed');
+    return Q.all(platforms.map(getPlatformInfo))
+        .then(outs => outs.join('\n\n'));
 }
 
 function listPlugins (projectRoot) {
     var pluginPath = path.join(projectRoot, 'plugins');
-    var plugins = cordova_util.findPlugins(pluginPath);
-
-    if (!plugins.length) {
-        return Q.reject('No Plugins Currently Installed');
-    }
-    return Q('Plugins: \n\n' + plugins);
+    var plugins = cordova_util.findPlugins(pluginPath).join('\n');
+    return 'Plugins:' + (plugins.length ? '\n' + indent(plugins) : ' []');
 }
 
 function getProjectConfig (projectRoot) {
-    if (!fs.existsSync(projectRoot)) {
-        return Q.reject('Config.xml file not found');
+    const configPath = cordova_util.projectConfig(projectRoot);
+    if (!fs.existsSync(configPath)) {
+        return 'config.xml file not found';
     }
-    return Q('Config.xml file: \n\n' + (fs.readFileSync(cordova_util.projectConfig(projectRoot), 'utf-8')));
+    const config = fs.readFileSync(configPath, 'utf-8');
+    return `config.xml <<EOF\n${config}\nEOF`;
 }
