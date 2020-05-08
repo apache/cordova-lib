b73bb631e3dcd3577ae11c6b6990198e57fe9714
Erisu <ellis.bryan@gmail.com>
2020-05-08 22:07:29 +0900

    breaking: upgrade cordova dependencies for next major

--- spec/cordova/util.spec.js
+++ spec/cordova/util.spec.js
@@ -104,7 +104,7 @@ describe('util module', function () {
 
             return helpers.getFixture('androidApp').copyTo(platformPath)
                 .then(_ => util.getInstalledPlatformsWithVersions(temp))
-                .then(versions => expect(versions[PLATFORM]).toBe('8.1.0'));
+                .then(versions => expect(versions[PLATFORM]).toBe('9.0.0-dev'));
         });
     });
     describe('findPlugins method', function () {
