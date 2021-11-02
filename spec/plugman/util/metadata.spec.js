aafac43626942078cf238a25ac1404343697a3cc
Erisu <erisu@apache.org>
2021-11-02 18:20:22 +0900

    fix(lint): slient array-callback-return warning from metadata mock test

--- spec/plugman/util/metadata.spec.js
+++ spec/plugman/util/metadata.spec.js
@@ -32,6 +32,7 @@ const fsMock = {
 };
 
 // expect fsMock to only operate on fetchJsonPath
+// eslint-disable-next-line
 Object.entries(fsMock).map(([key, fn]) => {
     fsMock[key] = (...args) => {
         expect(args[0]).toBe(fetchJsonPath);
